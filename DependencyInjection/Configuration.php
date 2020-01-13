6b7f0f1842e7b8371d3d72fadf503baee3e49e0a
Amorebietako Udala <informatika@amorebieta.eus>
2020-01-13 09:52:47 +0100

    - A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0. So this change is to make it work in SF5.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,8 +16,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fr3d_ldap');
+        $treeBuilder = new TreeBuilder('fr3d_ldap');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
